880a258bfe6d7eeb809537f7f7544f573c23cf5b
Costa Huang <costa.huang@outlook.com>
2018-10-22 01:47:52 -0400

    Enable unmarshall of PortWardenElement objects

--- cmd/portwarden/main.go
+++ cmd/portwarden/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/vwxyzjn/portwarden"
 )
 
@@ -16,13 +19,48 @@ const (
 )
 
 func main() {
-	test := &portwarden.PortWardenElement{}
+	test := []portwarden.PortWardenElement{}
 	fmt.Println(test)
 
-	var stdout, stderr bytes.Buffer
+	sessionKey := BWUnlockVaultToGetSessionKey()
+	rawByte := BWListItemsRawBytes(sessionKey)
+	if err := json.Unmarshal(rawByte, &test); err != nil {
+		panic(err)
+	}
+	spew.Dump(test[:5])
+}
 
-	fmt.Println("Please enter your master password: (input is hidden)")
+func extractSessionKey(stdout string) string {
+	r := regexp.MustCompile(`BW_SESSION=".+"`)
+	sessionKeyRawString := r.FindAllString(stdout, 1)[0]
+	sessionKey := strings.TrimPrefix(sessionKeyRawString, `BW_SESSION="`)
+	sessionKey = sessionKey[:len(sessionKey)-1]
+	return sessionKey
+}
+
+func BWUnlockVaultToGetSessionKey() string {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
 	cmd := exec.Command("bw", "unlock")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	os.Stdout = rescueStdout
+	return extractSessionKey(string(out))
+}
+
+func BWListItemsRawBytes(sessionKey string) []byte {
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("bw", "list", "items", "--session", sessionKey)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
@@ -30,15 +68,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(extractSessionKey(string(stdout.Bytes())))
-}
-
-func extractSessionKey(stdout string) string {
-	r := regexp.MustCompile(`BW_SESSION=".+"`)
-	sessionKeyRawString := r.FindAllString(stdout, 1)[0]
-	fmt.Println(sessionKeyRawString)
-	sessionKey := strings.TrimPrefix(sessionKeyRawString, `BW_SESSION="`)
-	sessionKey = sessionKey[:len(sessionKey)-1]
-	return sessionKey
+	return stdout.Bytes()
 }
