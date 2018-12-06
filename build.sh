e7a8c23fcbec8dd1de9e41231cc110b71a43ba81
Costa Huang <costa.huang@outlook.com>
2018-12-06 11:13:28 -0500

    Master # Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 ## The folloiwng commands should be executed separately.
 
-docker build -t vwxyzjn/portwarden-scheduler:0.0.1 .
+# Build the production image
+docker build -t vwxyzjn/portwarden-server-prod:1.7.0 -f Dockerfile.Build .
+
+# Build the development image
+docker build -t vwxyzjn/portwarden-server-dev:1.7.0 .
 
 # minikube docker-env
-
-docker-compose up 
